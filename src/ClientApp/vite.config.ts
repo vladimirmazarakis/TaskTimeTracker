002ea52215fabd5e2b7f49206bf1e9c1259a9aee
unknown <vladimirmazarakis@hotmail.com>
2025-07-08 11:57:28 +0300

    Added static copy plugin for staticwebapp.config.json

--- src/ClientApp/vite.config.ts
+++ src/ClientApp/vite.config.ts
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'staticwebapp.config.json',
+          dest: '.', // Copies to dist/
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
